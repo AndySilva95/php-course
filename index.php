c2f7eb2b707451656ceb9c59535a54d7c5101b07
AndySilva95 <52051849+AndySilva95@users.noreply.github.com>
2021-01-11 10:42:48 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -14,7 +14,12 @@
 		$products['Computer']=750;
 	    $products['Car']=15000;
 	    $products['iPhone']=1000;
-	    $products['Toaster']=75;
+        $products['Toaster']=75;
+        $amount=800;
+        $taxRate=0.0825;
+        $addedTax= $amount*$taxRate;
+
+        echo $addedTax;
 
 	    foreach($products as $key => $value){
 		    echo "<p>The ".$key." costs ".$value."</p>";
@@ -27,6 +32,14 @@
                     echo "<p>".$key."</p>";
                 }
             }
+
+        function tax_calc ($amount,$tax){
+            $calculate_tax = $amount*$tax;
+            $amount = round($amount+$calculate_tax,2);
+            return $amount;
+
+        echo tax_calc(750,0.223);
+        }
 
     ?>
   </body>
